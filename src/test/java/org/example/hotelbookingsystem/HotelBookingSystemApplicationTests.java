2820ab3a83cce61277321c4e709652a43d7a0e7b
chansoklin <chansoklin20@gmail.com>
2026-05-09 12:21:51 +0700

    Complete Hotel Booking System with Frontend and Backend

--- src/test/java/org/example/hotelbookingsystem/HotelBookingSystemApplicationTests.java
+++ src/test/java/org/example/hotelbookingsystem/HotelBookingSystemApplicationTests.java
@@ -1,9 +1,10 @@
 package org.example.hotelbookingsystem;
 
+import com.hotel.bookingsystem.HotelBookingSystemApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HotelBookingSystemApplication.class)
 class HotelBookingSystemApplicationTests {
 
     @Test
